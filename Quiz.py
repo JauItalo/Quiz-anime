0649a5337e8008c1ffa178b0b7d771b7c52828d3
jau italo <jau.batista@dcx.ufpb.br>
2024-10-25 19:48:08 -0300

    remodelando para usar o banco de dados SQL MANAGENTMENT, pois tava dando erro de coockies

--- Quiz.py
+++ Quiz.py
@@ -1,10 +1,12 @@
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import json
+import random
 
 app = Flask(__name__)
+app.secret_key = 'kRxSUCjUBwNcVgPA3w1U'
 
-def load_questions():
+def fetch_questions():
     with open('questions.json', encoding='utf-8') as file:
         return json.load(file)['questions']
     
@@ -12,11 +14,24 @@ def load_questions():
 
 @app.route('/')
 def index():
-    return redirect(url_for('quiz', question_num=0, score=0))
+    questions = fetch_questions()
+    session['questions'] = random.sample(questions, len(questions))
+    session['question_num'] = 0
+    session['score'] = 0
+    return redirect(url_for('quiz'))
 
-@app.route('/quiz/<int:question_num>/<int:score>', methods=['GET', 'POST'])
-def quiz(question_num, score):
-    questions = load_questions()
+
+@app.route('/quiz', methods=['GET', 'POST'])
+def quiz():
+    if 'questions' not in session or not session['questions']:
+        return redirect(url_for('index'))
+
+    questions = session.get('questions')
+    question_num = session.get('question_num', 0)
+    score = session.get('score', 0)
+
+    if question_num >= len(questions):
+        return redirect(url_for('result', score=score))
 
 
     if request.method == 'POST':
@@ -25,13 +40,10 @@ def quiz(question_num, score):
 
         if user_answer == correct_answer:
             score += 10
+            session['score'] = score
 
-        question_num += 1
-
-        if question_num >= len(questions):
-            return redirect(url_for('result', score=score))
-        
-        return redirect(url_for('quiz', question_num=question_num, score=score))
+        session['question_num'] = question_num + 1
+        return redirect(url_for('quiz'))
     
     question = questions[question_num]
     return render_template('quiz.html', question=question, question_num=question_num, score=score)
